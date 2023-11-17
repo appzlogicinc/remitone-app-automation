77864fe2cf1d2799ddcf71216190210b7cc052fd
Swasti Sharma <swasti.sharma@appslogic.com>
2023-11-17 19:45:51 +0530

    updated beneficiary file

--- test/pageobjects/beneficiary.page.js
+++ test/pageobjects/beneficiary.page.js
@@ -120,4 +120,5 @@ class BeneficiaryPage {
 
 }
 
+
 export default new BeneficiaryPage();
--- test/specs/beneficiary.js
+++ test/specs/beneficiary.js
@@ -14,6 +14,7 @@ describe('Verify Benificiary page', () => {
         await BeneficiaryPage.clickSaveBtn();
     })
 
+
     
 })
 
