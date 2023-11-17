12703a8601f90e51ccf3a9e3c97a4158fb90e763
Swasti Sharma <swasti.sharma@appslogic.com>
2023-11-17 14:15:25 +0530

    pushing login pageobject and specs files

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -8,34 +8,95 @@ class LoginPage {
     /**
      * define selectors using getter methods
      */
-    get inputUsername () {
-        return $('#username');
-    }
+    // get ignoreBtn (){
+    //     return $('IGNORE');
+    // }
 
-    get inputPassword () {
-        return $('#password');
-    }
+    // get loginBtn(){
+    //     return $('[name="Existing User? Login"]');
+    // }
 
-    get btnSubmit () {
-        return $('button[type="submit"]');
-    }
+    // get inputUsername () {
+    //     return $('#username');
+    // }
+
+    // get inputPassword () {
+    //     return $('#password');
+    // }
+
+    // get btnSubmit () {
+    //     return $('button[type="submit"]');
+    // }
 
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
+
+    async clickDismissPopup (){
+        const selector = 'new UiSelector().text("IGNORE").className("android.widget.Button")'
+        const button = await $(`android=${selector}`)
+        await button.click()
+      // await this.ignoreBtn().click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    // open () {
-    //     return super.open('login');
+    async clickLoginBtn(){
+        const selector = 'new UiSelector().description("Existing User? Login")';
+        const button = await $(`android=${selector}`);
+        await button.click();
+       // await this.loginBtn().click();
+    }
+
+    async login ( username,  password) {
+        const selector1 = 'new UiSelector().textContains("Email Address ")';
+        const inputUsername = await $(`android=${selector1}`);
+
+        const selector2 = 'new UiSelector().textContains("Password ")';
+        const inputPassword = await $(`android=${selector2}`);
+
+        const selector3 = 'new UiSelector().description("Login")';
+        const btnSubmit = await $(`android=${selector3}`);
+
+        await inputUsername.click();
+        await inputUsername.setValue(username);
+        await inputPassword.click();
+        await inputPassword.setValue(password);
+        await btnSubmit.click();
+    }
+
+     async isDahboardDisplayed() {
+        const element = $('android=new UiSelector().description("Wallet")');
+
+        return element.isExisting();
+     }
+
+    //  async isDahboardDisplayed(){
+    //     //const element = $('android=new UiSelector().description("Wallet")');
+
+    //     const selector = 'new UiSelector().descriptionContains("Wallet")';
+    //     const dashboardElement =  $(`android=${selector}`);
+    //     return  await dashboardElement.isExisting();
     // }
+
+      async clickMoreBtn(){
+        const selector = 'new UiSelector().description("More")';
+        const moreBtn = await $(`android=${selector}`);
+        moreBtn.click();
+   }
+
+      async clickLogoutBtn(){
+      const selector = 'new UiSelector().description("Logout")';
+      const logoutBtn = await $(`android=${selector}`);
+      logoutBtn.click();
+   }
+
+     async clickOkBtn(){
+     const selector = 'new UiSelector().description("OK")';
+     const okBtn = await $(`android=${selector}`);
+     okBtn.click();
+   }
+
+
 }
 
 export default new LoginPage();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,12 +1,20 @@
 import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+//import SecurePage from '../pageobjects/secure.page.js'
 
-describe('Remit One User Login', () => {
+describe('Remit One User Login and logout', () => {
     it('should login with valid credentials', async () => {
-        // await LoginPage.open()
+        await LoginPage.clickLoginBtn();
+        await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
+       // const isUserLoggedIn = LoginPage.isDahboardDisplayed();
+       // expect(isUserLoggedIn).to.be.true;
+    })
 
-        await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123')
+    it('should able logout', async () => {
+        await LoginPage.clickMoreBtn();
+        await LoginPage.clickLogoutBtn();
+        await LoginPage.clickOkBtn();
     })
 })
+
 
