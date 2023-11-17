011b59d68e144223bed1ef581139ca457e47159d
Swasti Sharma <swasti.sharma@appslogic.com>
2023-11-17 18:35:23 +0530

    updated beneficiary filee

--- test/pageobjects/beneficiary.page.js
+++ test/pageobjects/beneficiary.page.js
@@ -24,7 +24,7 @@ class BeneficiaryPage {
     }
 
     get inputLastname (){
-        const selector = 'new UiSelector().description("Last Name")'
+        const selector = 'new UiSelector().textContains("Last Name *")'
         return $(`android=${selector}`)
     }
 
@@ -58,15 +58,10 @@ class BeneficiaryPage {
         return $(`android=${selector}`)
     }
 
-    get paasport (){
+    get passport (){
         const selector8= 'new UiSelector().descriptionContains("PASSPORT")';
-        return $(`android=${selector}`)
-    }
-
-    get idType (){
-        const selector = 'new UiSelector().descriptionContains("Identification Type")';
-        return $(`android=${selector}`)
-    }
+        return $(`android=${selector}`);
+    } 
 
     get saveBbutton (){
         const selector = 'new UiSelector().description("Save")'
@@ -79,46 +74,48 @@ class BeneficiaryPage {
      */
 
     async clickBeneficiaryBtn(){
-        await beneficiaryBtn.click()
+        await this.beneficiaryBtn.click()
     }
     
     async clickAddbeneficiaryBtn(){
-        await addBeneficiaryBtn.click()
+        await this.addBeneficiaryBtn.click()
     }
 
      async enterDetails(Firstname,Lastname,mobileNo,address,city,state,postcode){
 
-        await inputFirstname.click();
-        await inputFirstname.setValue(Firstname);
+        await this.inputFirstname.click();
+        await this.inputFirstname.setValue(Firstname);
         browser.hideKeyboard();
         const scrollableContainer = $('android=new UiScrollable(new UiSelector().scrollable(true))');
         scrollableContainer.scroll('down');
-        await inputLastname.click();
-        await inputLastname.setValue(Lastname);
+        await this.inputLastname.click();
+        await this.inputLastname.setValue(Lastname);
         browser.hideKeyboard();
         scrollableContainer.scroll('down');
-        await inputMobileno.click();
-        await inputMobileno.setValue(mobileNo);
+        await this.inputMobileno.click();
+        await this.inputMobileno.setValue(mobileNo);
         browser.hideKeyboard();
-        await enterAddress.click();
-        await enterAddress.setValue(address);
+        await this.enterAddress.click();
+        await this.enterAddress.setValue(address);
         browser.hideKeyboard();
-        await enterCity.click();
-        await enterCity.setValue(city);
+        await this.enterCity.click();
+        await this.enterCity.setValue(city);
         browser.hideKeyboard();
-        await enterState.click();
-        await enterState.setValue(state);
+        scrollableContainer.scroll('down');
+        await this.enterState.click();
+        await this.enterState.setValue(state);
         browser.hideKeyboard();
-        await enterPostcode.click();
-        await enterPostcode.setValue(postcode);
+        scrollableContainer.scroll('down');
+        await this.enterPostcode.click();
+        await this.enterPostcode.setValue(postcode);
         browser.hideKeyboard();
-        await idType.click();
-        await  paasport.click();
+        await this.idType.click();
+        await this.passport.click();
 
     }
 
    async  clickSaveBtn(){
-    await saveBbutton.click();
+    await this.saveBbutton.click();
     }
 
 }
