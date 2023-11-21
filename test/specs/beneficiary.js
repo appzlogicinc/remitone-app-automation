a7d96b4778a0b394af9e79cf5ae8f5ab4f0a5fd7
Shadab Anwar <apple@apples-MacBook-Pro.local>
2023-11-21 16:19:54 +0530

    updated

--- test/specs/beneficiary.js
+++ test/specs/beneficiary.js
@@ -2,12 +2,11 @@ import { expect } from '@wdio/globals'
 import BeneficiaryPage from '../pageobjects/beneficiary.page.js'
 import LoginPage from '../pageobjects/login.page.js'
 
-//import SecurePage from '../pageobjects/secure.page.js'
 
 describe('Verify Benificiary page', () => {
     it('add beneficiary', async () => {
-        await LoginPage.clickLoginBtn();
-        await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
+       // await LoginPage.clickLoginBtn();
+        //await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
         await BeneficiaryPage.clickBeneficiaryBtn();
         await BeneficiaryPage.clickAddbeneficiaryBtn();
         await BeneficiaryPage.enterBeneficiaryDetails("rock","smith","1234567890","test","test","test","123456");
--- test/specs/login.js
+++ test/specs/login.js
@@ -2,22 +2,12 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 
 
-describe('User Login', () => {
-    it('User should login with valid credentials', async () => {
-        await LoginPage.clickLoginBtn();
-        await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
-
-    })
-
- 
-});
-
 describe('User logout', () => {
 
     it('User should be able to logout ', async () => {
         await LoginPage.clickLoginBtn();
         await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
-        //await LoginPage.clickMoreBtn();
+        await LoginPage.clickMoreBtn();
         await LoginPage.clickLogoutBtn();
         await LoginPage.clickOkBtn();
     })
