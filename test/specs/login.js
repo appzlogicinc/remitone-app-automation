94f0bdc82410d2d6678fecc730f2f6c9eacce232
snehappzlozic <sneha.mavuri@appzlogic.com>
2023-11-28 09:57:32 +0530

    commit

---
+++ test/specs/login.js
@@ -0,0 +1,19 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+
+
+describe('User logout', () => {
+
+    it('User should be able to logout ', async () => {
+        await LoginPage.clickLoginBtn();
+        await LoginPage.login('dhawal@appzlogic.com', 'Appzlogic@123');
+        await LoginPage.clickMoreBtn();
+        await LoginPage.clickLogoutBtn();
+        await LoginPage.clickOkBtn();
+    })
+});
+
+
+
+
+
