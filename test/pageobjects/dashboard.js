e19af540fc8ffb49c78290765e354a23605f2423
snehappzlozic <sneha.mavuri@appzlogic.com>
2023-12-04 20:42:17 +0530

    completed the scenario of verify user is able to send money to end user from homescreen

--- test/pageobjects/dashboard.js
+++ test/pageobjects/dashboard.js
@@ -7,10 +7,13 @@ class DashboardPage {
         return $(`android=${selector}`);
     }
 
-    get sourceCurrencyDropdown() {
-        const selector = 'new UiSelector().description("GBP")';
-        return $(`android=${selector}`);
-    }
+get sourceCurrencyDropdown()  {
+    const selector = 'new UiSelector().description("GBP")';
+    return $(`android=${selector}`);
+}
+
+
+    
 
     get sourceCurrencyField(){
         const selector = 'new UiSelector().description("GBP")';
@@ -33,11 +36,92 @@ class DashboardPage {
     }
 
     get selectBeneficiaryScreen(){
-        const selector = 'new UiSelector().description("Select Beneficiary")';
+        const selector = 'new UiSelector().descriptionContains("Select Beneficiary")';
         return $(`android=${selector}`);
     }
 
-    async clickSendNowButton() {
+    get  beneficaryOption(){
+        const selector = 'new UiSelector().descriptionContains("ABC")';
+        return $(`android=${selector}`);
+
+    }
+
+    get  transactionTypeOption(){
+        const selector = 'new UiSelector().descriptionContains("Select Transaction Type")';
+        return $(`android=${selector}`);
+
+    }
+
+    get  selectCardTransfer(){
+        const selector = 'new UiSelector().descriptionContains("Card Transfer")';
+        return $(`android=${selector}`);
+
+    }
+
+    get cardNumber(){
+        const selector = 'new UiSelector().textContains("234567, Card Number *")';
+        return $(`android=${selector}`);
+    }
+
+    get sourceOfIncome(){ //dropdown
+        const selector = 'new UiSelector().descriptionContains("Source of Income *")';
+        return $(`android=${selector}`);
+    }
+
+    get businessOptionFromDropdown(){
+        const selector = 'new UiSelector().descriptionContains("Business")';
+        return $(`android=${selector}`);
+
+    }
+
+    get purposeOfRemitance(){ //dropdown
+        const selector = 'new UiSelector().descriptionContains("Purpose of Remittance *")';
+        return $(`android=${selector}`);
+    }
+
+    get familyupportOptionFromDropdown(){
+        const selector = 'new UiSelector().descriptionContains("Family Support")';
+        return $(`android=${selector}`);
+
+    }
+
+    get clickNextButton() {
+        const selector = 'new UiSelector().description("Next")';
+        return $(`android=${selector}`);
+    }
+
+
+    get selectPaymentMethodScreen(){
+        const selector = 'new UiSelector().descriptionContains("Select Payment Method")';
+        return $(`android=${selector}`);
+    }
+
+    get selectOnBankTransferOption(){
+        const selector = 'new UiSelector().descriptionContains("Bank Transfer")';
+        return $(`android=${selector}`);
+    }
+
+    get verifyCompleteTransactionScreenDisplayed(){
+        const selector = 'new UiSelector().descriptionContains("Complete Transaction")';
+        return $(`android=${selector}`);
+    }
+
+    get amountTextFieldInFinalPaymentScreen(){
+        const selector = 'new UiSelector().text("Amount")';
+        return $$(`android=${selector}`);
+    }
+
+    get clickOnSendNowFinalPaymentScreen(){
+        const selector = 'new UiSelector().text("Send Now")';
+        return $$(`android=${selector}`);
+    }
+
+
+
+
+
+
+async clickSendNowButton() {
         await this.sendNowButton.click();
     }
 
@@ -48,9 +132,6 @@ class DashboardPage {
     }
 
     async verifyDestinationCountryDisplayed(){
-        // await this.destinationCountryDropdown.waitForExist({ timeout: 5000 });
-
-        // return (await this.destinationCountryDropdown).isDisplayed();
         async function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
           }
@@ -72,7 +153,74 @@ class DashboardPage {
     async verifySelectBeneficiaryScreen(){
         return (await this.selectBeneficiaryScreen).isDisplayed();
     }
+
     
+
+    
+
+    async verifySelectedBeneficiaryOption(){
+        await this.beneficaryOption.click();
+}
+
+async verifyTransactionTypeScreen(){
+    return (await this.transactionTypeOption).isDisplayed();
+}
+
+async clickOnCardTransfer(){
+    await this.selectCardTransfer.click();
+}
+
+
+
+async sourceOfIncomeDropdown(){
+    await this.sourceOfIncome.click();
+}
+
+
+async clickOnBusinessOptionFromDropdown(){
+    await this.businessOptionFromDropdown.click();
+}
+
+async clickOnPurposeDropdown(){
+    (await this.purposeOfRemitance).click();
+}
+
+async clickOnFamilySupportOption(){
+    (await this.familyupportOptionFromDropdown).click();
+}
+
+async clickOnNextButton() {
+    await this.clickNextButton.click();
+}
+
+async verifyPaymentMethodScreen(){
+    async function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      console.log('Start');
+      await sleep(9000);
+      console.log('End');
+    return (await this.selectPaymentMethodScreen).isDisplayed();
+}
+
+async clickOnBankTransfer(){
+    await this.selectOnBankTransferOption.click();
+}
+
+async completeTransactionScreen(){
+    return await this.verifyCompleteTransactionScreenDisplayed.isDisplayed();
+}
+
+async enterAmountInTextFieldInFinalPaymentScreen() {
+    await this.amountTextFieldInFinalPaymentScreen[0].click();
+    await this.amountTextFieldInFinalPaymentScreen[0].setValue('1.5');
+
+}
+
+async clickSendNowButtonOnFinalPaymentScreen() {
+    await this.clickOnSendNowFinalPaymentScreen.click();
+}
+
 
 
 
--- test/specs/dashboard.test.js
+++ test/specs/dashboard.test.js
@@ -6,7 +6,6 @@ import { expect as chaiExpect, should } from 'chai';
 describe('Dashboard', () => {
 
     it('verify user is able to send money to end user from home-screen', async () => {
-        
         await LoginPage.clickLoginBtn();
         await LoginPage.login('swasti.sharma@appzlogic.com', 'Appzlogic@123');
         chaiExpect(await DashboardPage.verifyDestinationCountryDisplayed()).to.be.true;
@@ -14,42 +13,24 @@ describe('Dashboard', () => {
         await DashboardPage.enterAmountInTextField();
         await DashboardPage.clickSendNowButton();
         chaiExpect(await DashboardPage.verifySelectBeneficiaryScreen()).to.be.true;
+        await DashboardPage.verifySelectedBeneficiaryOption();
+        chaiExpect(await DashboardPage.verifyTransactionTypeScreen()).to.be.true;
+        await DashboardPage.clickOnCardTransfer();
+        await DashboardPage.sourceOfIncomeDropdown();
+        await DashboardPage.clickOnBusinessOptionFromDropdown();
+        await DashboardPage.clickOnPurposeDropdown();
+        await DashboardPage.clickOnFamilySupportOption();
+        await DashboardPage.clickOnNextButton();
+        chaiExpect(await DashboardPage.verifyPaymentMethodScreen).to.be.true;
+        await DashboardPage.clickOnBankTransfer();
+        chaiExpect(await DashboardPage.completeTransactionScreen).to.be.true;
+        await DashboardPage.enterAmountInTextFieldInFinalPaymentScreen();
+        await DashboardPage.clickSendNowButtonOnFinalPaymentScreen();
+
+
+
+
 
 
     })
 });
-
-
-
-// describe('Currency Converter Dashboard', () => {
-//     it('should select destination country from the dropdown', async () => {
-//         await Dashboard.selectDestinationCountry('Your Destination Country');
-        
-//         await Dashboard.selectDestinationCountry('India');
-
-//         // const selectedCountry = await Dashboard.getSelectedDestinationCountry();
-//         // expect(selectedCountry).to.equal('Your Destination Country');
-//     });
-
-//     it('should select source currency from the dropdown', async () => {
-//         await Dashboard.selectSourceCurrency('Your Source Currency');
-
-//         const selectedSourceCurrency = await CurrencyConverterPage.getSelectedSourceCurrency();
-//         expect(selectedSourceCurrency).to.equal('Your Source Currency');
-//     });
-
-//     it('should select target currency from the dropdown', async () => {
-//         await Dashboard.selectTargetCurrency('Your Target Currency');
-
-//         const selectedTargetCurrency = await Dashboard.getSelectedTargetCurrency();
-//         expect(selectedTargetCurrency).to.equal('Your Target Currency');
-//     });
-
-//     it('should click on the submit button', async () => {
-//         await Dashboard.clickSubmitButton();
-
-//         // Add assertions or verifications as needed
-//         const successMessage = await Dashboard.getSuccessMessage();
-//         expect(successMessage).to.equal('Conversion Successful');
-//     });
-// });
